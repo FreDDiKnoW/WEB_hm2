6298decd8a7ef331acd7fec12350af2ae1466ac9
Fedir <klumenko356@gmail.com>
2025-09-17 09:29:19 +0300

    Main page HTML code + random choice for Where_To-Go (vibe coding)

--- PlacesToGO/places/urls.py
+++ PlacesToGO/places/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.index1, name='index'),
     path('all_places/', views.all_places, name='all_places'),
     path('add_place/', views.add_place, name='add_place'),
-    path('where_to_go/', views.where_to_go, name='where_to_go'),
 ]
--- PlacesToGO/places/views.py
+++ PlacesToGO/places/views.py
@@ -1,19 +1,42 @@
 from django.shortcuts import render
 from .models import Place
+import random
 
 
 def index1(request):
-    return render(request, 'places/index1.html')
+    places = Place.objects.all()
+    places_count = places.count()
+
+    show_random = request.GET.get('random', False)
+
+    if not places.exists():
+        return render(request, 'places/index1.html', {
+            'place': None,
+            'places_count': places_count
+        })
+
+    if show_random:
+        weighted_places = []
+        for place in places:
+            weighted_places.extend([place] * place.rating)
+
+        random_place = random.choice(weighted_places)
+
+        return render(request, 'places/index1.html', {
+            'place': random_place,
+            'places_count': places_count
+        })
+    else:
+        return render(request, 'places/index1.html', {
+            'place': None,
+            'places_count': places_count
+        })
 
 
 def all_places(request):
-    Places = Place.objects.all()
-    return render(request, 'places/all_places.html', {'title': 'WhereToGO' , 'places': Places})
+    places = Place.objects.all()
+    return render(request, 'places/all_places.html', {'title': 'WhereToGO', 'places': places})
 
 
 def add_place(request):
     return render(request, 'places/add_place.html')
-
-
-def where_to_go(request):
-    return render(request, 'places/index4.html')
